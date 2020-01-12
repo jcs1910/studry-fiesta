45f31a240e545454c68f820351d242a9a4d4544f
Chang-suk <jcs191072@gmail.com>
2020-01-13 01:59:55 +0900

    Generate and validate token

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -4,8 +4,8 @@ import User from '../../models/user';
 /*
  POST /api/auth/register
  {
-  username: 'velopert',
-  password: 'mypass123'
+  username: 'changsuk',
+  password: 'password1234'
  }
 */
 
@@ -23,7 +23,7 @@ export const register = async ctx => {
   if (result.error) {
     ctx.status = 400;
     ctx.body = result.error;
-    reutrn;
+    return;
   }
   const { username, password } = ctx.request.body;
   try {
@@ -35,24 +35,76 @@ export const register = async ctx => {
     }
 
     const user = new User({
-      username,
+      username
     });
     await user.setPassword(password); // 비밀번호 설정
     await user.save(); //데이터베이스에 저장
 
     // 응답할 데이터에서 hashedPassword 필드 제거
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
 };
 
+/*
+ POST /api/auth/login
+ {
+  username:'changsuk',
+  password:'password1234'
+ }
+ */
+
 export const login = async ctx => {
-  // 로그인
+  const { username, password } = ctx.request.body;
+
+  // username, password가 없으면 에러처리
+  if (!username || !password) {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  try {
+    const user = await User.findByUsername(username);
+    // 계정이 존재하지 않으면 에러 처리
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    // 잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+      const token = user.generateToken();
+      ctx.cookies.set('access_token', token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+        httpOnly: true,
+      });
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
+/*
+  GET /api/auth/check
+*/
+
 export const check = async ctx => {
-  // 로그인 상태 확인
+  const { user } = ctx.state;
+  if (!user) {
+    // 로그인 중 아님
+    ctx.status = 401 // Unauthorized
+    return;
+  }
+  ctx.body = user;
 };
 
 export const logout = async ctx => {
